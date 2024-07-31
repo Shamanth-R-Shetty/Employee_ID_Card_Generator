2f768c0dba0fdbdf6ff8f33f30e40f69be837e70
Shamanth-R-Shetty <162606044+Shamanth-R-Shetty@users.noreply.github.com>
2024-07-31 19:00:06 +0530

    Delete EmployeesRepository.java

--- EmployeesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.boostmytool.bestemp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeesRepository extends JpaRepository<Employee, Integer> {
-    
-
-}
